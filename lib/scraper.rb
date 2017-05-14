f872351823c11b3f38f113cc9430411f297ee8a0
Beastie PC <gskondratenko@opora.org.ua>
2017-05-14 22:03:46 +0300

    fix parser metod scrape

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require_relative './people'
 
 class ScrapeMp
-  def parse
+  def parser
     DataMapper.auto_upgrade!
     url = "https://www.lvivrada.gov.ua/deputaty/deputaty-miskoi-rady"
     page = get_page(url)
